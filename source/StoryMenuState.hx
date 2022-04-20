fb627eebe105d4c19953d08bcc01d948c29bae65
pasta870 <96292419+pasta870@users.noreply.github.com>
2022-04-20 11:32:05 +0800

    Update StoryMenuState.hx

--- source/StoryMenuState.hx
+++ source/StoryMenuState.hx
@@ -179,6 +179,10 @@ class StoryMenuState extends MusicBeatState
 
 		changeWeek();
 
+                #if android
+	        addVirtualPad(FULL, A_B);
+                #end
+
 		super.create();
 	}
 
